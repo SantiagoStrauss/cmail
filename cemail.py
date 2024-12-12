f27b208df8ea79c32d04889e1c55784a422c4d87
SantiagoStrauss <ds.ramos@uniandes.edu.co>
2024-12-12 13:43:14 -0500

    workers 3

--- cemail.py
+++ cemail.py
@@ -85,6 +85,8 @@ class CompromisedEmailScraper:
         # Performance options
         options.add_argument('--disable-gpu')
         options.add_argument('--disable-extensions')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--no-sandbox') 
         options.add_argument('--disable-software-rasterizer')
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
         
